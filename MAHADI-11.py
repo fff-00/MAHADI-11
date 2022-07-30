0eb8a37b78baba249343e14b631cedb42169b792
fff-00 <106494973+fff-00@users.noreply.github.com>
2022-07-30 13:00:13 +0600

    Update and rename Mahadi-11 to MAHADI-11.py

---
+++ MAHADI-11.py
@@ -0,0 +1,13 @@
+
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from mahadi11 import menu
+    menu()
+ 
